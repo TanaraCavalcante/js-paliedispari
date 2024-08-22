bfb10b1882177e6bc3d050b6fd40326bcdd54b95
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-22 16:43:45 -0400

    error

--- JS/script01.js
+++ JS/script01.js
@@ -8,30 +8,27 @@ const btnReverse = document.querySelector('buttton');
 const result = document.querySelector('p');
 
 //TODO ELABORAZIONE
+//resgatando il valore passado per l'utente
+let word = wordField.value;
+
 //Elaborare la funzione
 let wordReverse = '';
 
 function getReverse(){
- for (let i = wordField.length -1; i >=0 ; i--){
-    wordReverse += wordField[i];
+ for (let i = word.length -1; i >=0 ; i--){
+    wordReverse += word[i];
     //condizione
-    let message = 'Le parole non sono Palidroma!';
-    if (wordField === wordReverse){
-        message = 'Le parole sono Palidroma!'
+    let message = '';
+    if (word === wordReverse){
+        message = 'Le parole sono <string>Palidroma!</string>'
     }
  }
-
  //! Perchè non vedo il "consol.log" em console?
- console.log(wordField);
+ console.log(word);
  console.log(wordReverse);
  console.log(message);  
- return ;
+
 }
 
-//TODO OUTPUT
 //Creare l'evento di reazione al 'click'
-btnReverse.addEventListener('click', function(getReverse){ //! Perchè "getReverse" è spento?
-    result.innerHTML = `<p>${wordField} ${wordReverse}</p>
-                        <p>${message}</p>`;
-})
-
+btnReverse.addEventListener('click', getReverse);
