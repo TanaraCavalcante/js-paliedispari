ca8f89c9f399f80c1baa86c33ef3113084071954
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-22 15:16:50 -0400

    recuperi del DOM

--- JS/script01.js
+++ JS/script01.js
@@ -2,4 +2,9 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
+//TODO RICUPERARE ELEMENTI DAL DOM
+const wordField = document.querySelector('input');
+const btnReverse = document.querySelector('buttton');
+const result = document.querySelector('p');
 
+//TODO ELABORAZIONE
