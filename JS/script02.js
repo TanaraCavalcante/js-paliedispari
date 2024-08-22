9403ddb336b7b69ec4331db4e710bab780c93c20
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-08-22 17:19:21 -0400

    script per "Pari o dispari"

---
+++ JS/script02.js
@@ -0,0 +1,24 @@
+/*PALI O DISPARI
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+//TODO RICUPERARE ELEMENTI DAL DOM
+const result2 = document.getElementById('result2');
+
+//TODO RACCOLTA DATI
+const question = parseInt(prompt('Pari o Dispari?'));
+
+//TODO CALCOLO
+//Generare i valori random sia per l'utente che per pc
+//Fare la sommatoria dei valori
+//Dire se il risultato è pari o dispari?
+
+//TODO CHI HA VINTO?
+
+//TODO OUTPUT
+
+
+ 
